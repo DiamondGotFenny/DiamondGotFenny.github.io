0be12a480fead371e0931839ff0b0ac66f1db8ab
diamondcn923 <diamondcn923@gmail.com>
2019-09-11 18:37:02 +0800

    UI updated

---
+++ js/index.js
@@ -0,0 +1,38 @@
+const backTopBtn = document.getElementById("backTopBtn");
+
+const scrollFunc = () => {
+  // Get the current scroll value
+  let y = window.scrollY;
+
+  // If the scroll value is greater than the window height,
+  //change the button class from hidden to show
+  if (y > 20) {
+    backTopBtn.className = "return-to-top show";
+  } else {
+    backTopBtn.className = "return-to-top hidden";
+  }
+};
+
+window.addEventListener("scroll", scrollFunc);
+
+const scrollBackTop = () => {
+  //scroll down pixels from top
+  const distance =
+    document.documentElement.scrollTop || document.body.scrollTop;
+
+  if (distance > 0) {
+    window.requestAnimationFrame(scrollBackTop);
+    window.scrollTo(0, distance - distance / 10);
+  }
+};
+
+backTopBtn.addEventListener("click", e => {
+  e.preventDefault();
+  scrollBackTop();
+});
+
+function smoothJump(ele) {
+  document.querySelector(ele).scrollIntoView({
+    behavior: "smooth"
+  });
+}
